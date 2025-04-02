25a10793262fd1ec159ba1acf72a8b63dd576ba6
dyamagishi <daiki.yamagishi@olduvai.jp>
2025-04-02 09:37:07 +0900

    Update circle radius in SVG for UI consistency
    - Changed the radius of the circle in the SVG from 10 to 5 to better fit the updated design requirements.

--- web/main.js
+++ web/main.js
@@ -4,7 +4,7 @@ import { ComfyButton } from "../../scripts/ui/components/button.js";
 
 const circleSvg = `
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-  <circle cx="12" cy="12" r="10" fill="gray" />
+  <circle cx="12" cy="12" r="5" fill="gray" />
 </svg>
 `
 
